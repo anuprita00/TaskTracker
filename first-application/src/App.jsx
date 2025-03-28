ea9130f6de8e2b5a71102fdf607152d15dec43b4
Anuprita <anupande84@gmail.com>
2025-03-28 22:10:38 +0530

    header

--- first-application/src/App.jsx
+++ first-application/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./App.css"
+import TaskForm from './components/TaskForm';
 
 const App = () => {
   return (
     <div className='app'>
+      <TaskForm />
       <main className='app_main'>
         <section className='task_column'>section1</section>
         <section className='task_column'>section2</section>
--- first-application/src/components/TaskForm.jsx
+++ first-application/src/components/TaskForm.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React from "react";
+import "./TaskForm.css";
+import Tag from './tag.jsx';
 
 const TaskForm = () => {
   return (
-    <div>
-        <header className='app_header'>
-            <form>
-                <input type='text' className='task_input'
-                placeholder='Enter your task'/>
+    <header className="app_header">
+      <form>
+        <input
+          type="text"
+          className="task_input"
+          placeholder="Enter your task"
+        />
 
-                <div className='task_form_bottom_line'>
-                    <button className='tag'>HTML</button>
-                    <button className='tag'>CSS</button>
-                    <button className='tag'>Javascript</button>
-                    <button className='tag'>React</button>
+        <section className="task_form_bottom_line">
+          <div>
+            <Tag tagName ="HTML"/>
+            <Tag tagName ="CSS"/>
+            <Tag tagName ="Javascript"/>
+            <Tag tagName ="React"/>
+          </div>
 
-                </div>
-            </form>
-        </header>
+          <div>
+            <select className="task_select">
+              <option value="todo">To Do</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
+            <button type="submit" className="task_submit">
+              Add Task
+            </button>
+          </div>
+        </section>
+      </form>
+    </header>
+  );
+};
 
-    </div>
-  )
-}
-
-export default TaskForm
+export default TaskForm;
