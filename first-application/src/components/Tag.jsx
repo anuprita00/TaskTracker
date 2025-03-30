16f8ea628a40dbf92817f8fcdc38f87b20dd349c
Anuprita <anupande84@gmail.com>
2025-03-30 14:00:16 +0530

    color tags

--- first-application/src/components/Tag.jsx
+++ first-application/src/components/Tag.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-import './Tag.css'
+import "./Tag.css";
 
-const Tag = ({tagName, selectTag}) => {
-  return <button type="button"
-  className="tag" onClick={() => selectTag(tagName)}>{tagName}</button>;
-}
+const Tag = ({ tagName, selectTag, selected }) => {
+  // set different color for different tags using in line style
+  const tagStyle = {
+    HTML: {backgroundColor: "#fda821"},
+    CSS: {backgroundColor: "#15D4C8"},
+    Javascript: {backgroundColor: "#ffd12c"},
+    React: {backgroundColor: "#4cdafc"},
+    default: {backgroundColor: "#f9f9f9"},
+  }
+
+  return (
+    <button type="button"
+     className="tag" 
+     style={selected ? tagStyle[tagName] : tagStyle.default}
+     onClick={() => selectTag(tagName)}>
+      {tagName}
+    </button>
+  );
+};
 
 export default Tag;
--- first-application/src/components/TaskForm.jsx
+++ first-application/src/components/TaskForm.jsx
@@ -6,7 +6,7 @@ const TaskForm = () => {
   const [taskData, setTaskData] = useState({
     task: "",
     status: "todo",
-    tags: []
+    tags: [],
   });
 
   // const [task, setTask] = useState("");
@@ -20,41 +20,49 @@ const TaskForm = () => {
   //   setStatus(e.target.value);
   // }
 
+  const checkTag = (tag) => {
+    return taskData.tags.some((item) => item === tag);
+  };
+
   const selectTag = (tag) => {
-    if(taskData.tags.some(item => item === tag)){ //checks if the tag exists in that array.
+    if (taskData.tags.some((item) => item === tag)) {
+      //checks if the tag exists in that array.
       // If the tag exists, it means the user is trying to deselect it.
-      const filterTags = taskData.tags.filter(item => item !== tag) //creates a new array without the selected tag.
-      setTaskData(prev => { //Then, we update the state using setTaskData(), removing the tag.
-        return {...prev, tags:filterTags}
-      })
-    } else{ 
-      setTaskData(prev => {  //updates the state with the new list of selected tags.
-        return{...prev,
-          tags:[...prev.tags, tag]} // Adds the new tag to the array.
-      })
+      const filterTags = taskData.tags.filter((item) => item !== tag); //creates a new array without the selected tag.
+      setTaskData((prev) => {
+        //Then, we update the state using setTaskData(), removing the tag.
+        return { ...prev, tags: filterTags };
+      });
+    } else {
+      setTaskData((prev) => {
+        //updates the state with the new list of selected tags.
+        return { ...prev, tags: [...prev.tags, tag] }; // Adds the new tag to the array.
+      });
     }
   };
   console.log(taskData.tags);
 
-  const handleChange = e => {
-    const {name , value} = e.target; // Extract 'name' and 'value' from the input/select
+  const handleChange = (e) => {
+    const { name, value } = e.target; // Extract 'name' and 'value' from the input/select
     // const name = e.target.name;
     // const value = e.target.value;
 
-    console.log(name, value)
+    console.log(name, value);
 
-    setTaskData(prev => {
-      return {...prev, // Keep existing state
-      [name]:value}  // Update the changed field dynamically
-    })
+    setTaskData((prev) => {
+      return {
+        ...prev, // Keep existing state
+        [name]: value,
+      }; // Update the changed field dynamically
+    });
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();  // Prevents page reload
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevents page reload
     console.log(e);
 
-    setTaskData({ task: "", status: "todo", tags:[] }); // Reset form after submission
-  }
+    setTaskData({ task: "", status: "todo", tags: [] }); // Reset form after submission
+  };
 
   return (
     <header className="app_header">
@@ -71,10 +79,11 @@ const TaskForm = () => {
 
         <section className="task_form_bottom_line">
           <div>
-            <Tag tagName="HTML" selectTag={selectTag} />
-            <Tag tagName="CSS" selectTag={selectTag}/>
-            <Tag tagName="Javascript" selectTag={selectTag}/>
-            <Tag tagName="React" selectTag={selectTag}/>
+            {/*pass true of false for each tag selected  */}
+            <Tag tagName="HTML" selectTag={selectTag} selected={"HTML"} />
+            <Tag tagName="CSS" selectTag={selectTag} selected={"CSS"} />
+            <Tag tagName="Javascript" selectTag={selectTag} selected={"Javascript"} />
+            <Tag tagName="React" selectTag={selectTag} selected={"React"} />
           </div>
 
           <div>
